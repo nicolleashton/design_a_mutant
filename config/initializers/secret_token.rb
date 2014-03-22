af2c3a74ec048650a2fac002d0ab53edf901f13b
ferg <joey@fergmedia.com>
2014-03-22 12:17:06 -0600

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+DesignAMutant::Application.config.secret_key_base = '60761ceb9898e61c22b62ec8f19859924f3e184d9585a0382c6720f6f4eb3db84a85b4cd34c908e240dedd5c040b859d6a17f636878b0e603ef8d8c4aa696374'
